5e8fd92e2c8077ab66b5982229c505c2fbdd8c2c
Marcos <marcosrm.docs@gmail.com>
2025-06-27 12:55:59 +0100

    feat: Add production server configuration, moving to render from strapicloud

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
+// config/server.ts
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
+    // A linha mais importante: diz ao Strapi para ler as chaves da variável de ambiente APP_KEYS.
+    // O 'env.array' é importante para o formato que a Render usa.
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    // Esta configuração é recomendada para produção
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
